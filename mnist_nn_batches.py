18e18a850be247f2613071445be7d94b9818d1f4
msoud2509 <msouders06@gmail.com>
2024-09-11 16:35:17 -0500

    add documentation

--- mnist_nn_batches.py
+++ mnist_nn_batches.py
@@ -4,8 +4,10 @@ import numpy as np
 #only if you want to train in batches, else use the superclass in mnist_nn.py
 class NN_batches(NeuralNetwork):
     def __init__(self, X, y, hiddenlayer_neurons, learning_rate, batch_size):
+        '''similar to NeuralNetwork class but now includes batch_size'''
         super().__init__(X, y, hiddenlayer_neurons, learning_rate)
-        '''x and y already shuffled from train test split, if want to shuffle again for batch split, use this code:
+        '''
+        x and y already shuffled from train test split, if want to shuffle again for batch split, use this code:
         
         perm = np.random.permutation(self.inputs.shape[1])
         self.inputs = self.inputs[:, perm]
@@ -15,6 +17,7 @@ class NN_batches(NeuralNetwork):
         self.batch_size = batch_size
 
     def batch_forward_pass(self, index):
+        '''splits input data into a batch before doing forward pass'''
         #set the begin and end of each batch before feedforward
         begin = index * self.batch_size
         end = min(begin + self.batch_size, self.inputs.shape[1] - 1)
@@ -23,6 +26,7 @@ class NN_batches(NeuralNetwork):
         super().fullforward_pass(X_batch)
     
     def batch_backward(self, index):
+        '''splits input data and output data into a batch before doing backward pass'''
         #set the begin and end of each batch before backprop
         begin = index * self.batch_size
         end = min(begin + self.batch_size, self.inputs.shape[1] - 1)
@@ -32,6 +36,7 @@ class NN_batches(NeuralNetwork):
         super().backward(X_batch, y_batch)
     
     def train_bybatch(self):
+        '''train of whole dataset but split using batches'''
         #loop ignores remaining batches after dividing into sets of equal size of batch_size
         for i in range(self.num_batches):
             self.batch_forward_pass(i)
